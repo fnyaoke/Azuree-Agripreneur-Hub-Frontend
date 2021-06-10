9e365dc10a9ea92b615c0f0b9d596e6c2d304e30
fnyaoke <fnyaoke@gmail.com>
2021-06-10 23:39:34 +0300

    restarted project

--- agripreneur/src/app/app.component.ts
+++ agripreneur/src/app/app.component.ts
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agripreneur';
-  welcome = 'Welcome to Azuree Farm World'
 }
--- agripreneur/src/app/navbar/navbar.component.spec.ts
+++
@@ -1,7 +0,0 @@
-import { NavbarComponent } from './navbar.component';
-
-describe('Navbar', () => {
-  it('should create an instance', () => {
-    expect(new NavbarComponent()).toBeTruthy();
-  });
-});
