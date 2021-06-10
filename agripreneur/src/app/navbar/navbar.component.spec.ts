23bc77a6d44cff0d2c2fb87753e9430fdeeb4a94
fnyaoke <fnyaoke@gmail.com>
2021-06-10 21:25:06 +0300

    created navbar component

---
+++ agripreneur/src/app/navbar/navbar.component.spec.ts
@@ -0,0 +1,7 @@
+import { NavbarComponent } from './navbar.component';
+
+describe('Navbar', () => {
+  it('should create an instance', () => {
+    expect(new NavbarComponent()).toBeTruthy();
+  });
+});
